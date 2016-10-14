561f8c882056c51e1310c8adf7c12fda90b30057
Nigel-Sultan <ns672@nau.edu>
2016-10-13 17:17:12 -0700

    Add files via upload

--- header.h
+++ header.h
@@ -2,7 +2,7 @@
 #define HEADER_H_
 
 typedef struct{
-  	int type; // 0 = camera, 1 = sphere, 2 = light
+  	int type; // 0 = camera, 1 = sphere, 2 = plane, 3 = light
   	union {
     	struct {
       		double width;
@@ -15,13 +15,19 @@ typedef struct{
       		double radius;
     	} sphere;
     	struct {
+    		double diffuse_color[3];
+    		double specular_color[3];
+      		double position[3];
+      		double normal[3];
+    	} plane;
+    	struct {
     		double color[3];
       		double position[3];
       		double direction[3];
       		double radial_a2;
       		double radial_a1;
       		double radial_a0;
-      		double angular_ao;
+      		double angular_a0;
     	} light;
   	};
 } Object;
